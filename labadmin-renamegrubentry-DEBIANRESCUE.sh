79e26e5360fe684fae8a806b16aed25a0bbc0e50
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-06-04 21:37:20 +0200

    Update labadmin-renamegrubentry-DEBIANRESCUE.sh

--- labadmin-renamegrubentry-DEBIANRESCUE.sh
+++ labadmin-renamegrubentry-DEBIANRESCUE.sh
@@ -1,3 +1,16 @@
+#!/usr/bin/env bash
+#===================================================================================
+# RENAME GRUB ENTRY TO DEBIANRESCUE
+#         FILE: labadmin-renamegrubentry-DEBIANRESCUE.sh
+#
+#  DESCRIPTION: Rename second partition (gpt2) to DEBIAN RESCUE 
+#
+#       AUTHOR: Leonardo Marco (labadmin@leonardomarco.com)
+#	   LICENSE: GNU General Public License v3.0
+#      VERSION: 2026.06
+#      CREATED: 2026.06.01
+#===================================================================================
+
 echo "LABADMIN: Renaming /boot/grub/grub.cfg menuentry sda2 to: DEBIAN RESCUE"
 mel=$(cat -n /boot/grub/grub.cfg | sed -n '/menuentry.*{$/,/}$/p' | grep -E "menuentry\b|set root=.*gpt2" | grep gpt2 -B1 | grep menuentry | awk '{print $1}' | head -1)
 [ "$mel" -eq "$mel" ] &>/dev/null || { echo "sda2 menuentry line not found"; exit 1; }
